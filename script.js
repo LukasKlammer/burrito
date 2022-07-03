f4524bab26df2f63265f7e72b96c567c0fadff19
unknown <klammer.lukas@hotmail.com>
2022-07-03 21:11:44 +0200

    render basket, add to basket logic

--- script.js
+++ script.js
@@ -20,26 +20,82 @@ let dishes = [
         price: 9.30
     },
     {
-        name: 'Pizza Gustonsa',
+        name: 'Pizza Gustosa',
         description: 'mit Oliven aus Süditalien',
         price: 8.70
     },
 ];
 
+let basketElements = [
+    {
+        name: 'Pizza Fitness',
+        description: 'mit frischem Gemüse aus biologischem Anbau',
+        price: 9.10,
+        amount: 3
+    },
+    {
+        name: 'Pizza Salute',
+        description: 'mit leckerem Brokkoli und Stracchino',
+        price: 9.30,
+        amount: 5
+    }
+];
+
 function init() {
-    renderdishes();
+    renderDishes();
+    renderBasket();
 }
 
-function addToBasket(dish) {
-    console.log('add to basket dish number ', dish);
-}
-
-function renderdishes() {
+function renderDishes() {
     let dishesContainer = document.getElementById('dishes');
     dishesContainer.innerHTML = '';
     for (let i = 0; i < dishes.length; i++) {
-        let dish = dishes[i];
-        console.log(dish);
+        const dish = dishes[i];
         dishesContainer.innerHTML += templateDish(dish);
     }
+}
+
+function renderBasket() {
+    let basketContainer = document.getElementById('basket-container');
+    basketContainer.innerHTML = '';
+    if (basketElements == 0) {
+        basketContainer.innerHTML = templateBasketEmpty()
+    } else {
+        renderBasketContent(basketContainer);
+    }
+}
+
+function renderBasketContent(basketContainer) {
+    for (let i = 0; i < basketElements.length; i++) {
+        const basketElement = basketElements[i];
+        basketContainer.innerHTML += templateBasket(basketElement);
+    }
+}
+
+/**
+ * 
+ * @param {string} name expects the name of the dish. Name must be unique
+ */
+function addToBasket(name) {
+    if (basketElements.find((item) => item.name === name)) {
+        console.log('Element bereits vorhanden');
+    } else {
+        console.log('Element muss noch richtig hinzugefügt werden... Problem amount');
+        // basketElements.push(findDish(name));
+        // console.log(basketElements);
+    }
+    renderBasket();
+}
+
+function increaseQuantity(name) {
+    console.log('Warenkorb erhöhen vom Element ', name);
+}
+
+function decreaseQuantity(name) {
+    console.log('Warenkorb verringern vom Element ', name);
+}
+
+function findDish(name) {
+    const dish = dishes.find((dish) => dish.name === name);
+    return dish;
 }
--- templates.js
+++ templates.js
@@ -1,5 +1,10 @@
+/**
+ * 
+ * @param {object} dish this object excpects a dish object with all informations about single dish
+ * @returns html template with variable content
+ */
 function templateDish(dish) {
-    priceWithKomma = dish['price'].toFixed(2).replace(".",",");
+    const priceWithKomma = dish['price'].toFixed(2).replace(".", ",");
     return /*html*/ `
     <div onclick="addToBasket('${dish['name']}')" class="dish">
         <h4>${dish['name']}</h4>
@@ -7,5 +12,31 @@ function templateDish(dish) {
         <span class="dish-price">${priceWithKomma} €</span>
         <img src="img/add.png" class="add-icon" alt="addToBasket">
     </div>
-`;
+    `;
+}
+
+function templateBasketEmpty() {
+    return /*html*/ `
+        <img class="basket-image" src="img/shopping-basket.png" alt="">
+        <h4>Fülle den Warenkorb</h4>
+        <p>Füge einige Gerichte aus der Speisekarte hinzu und bestelle dein Essen.</p>
+    `;
+}
+
+function templateBasket(basketElement) {
+    const sum = basketElement['price'] * basketElement['amount'];
+    const sumWithKomma = sum.toFixed(2).replace(".", ",");
+    return /*html*/ `
+        <div class="basket-element">
+            <div class="amount-name-price">
+                <span>${basketElement['amount']}</span>
+                <span>${basketElement['name']}</span>
+                <span>${sumWithKomma} €</span>
+            </div>
+            <div class="increase-decrease">
+                <img onclick="increaseQuantity('${basketElement['name']}')" class="increase-decrease-icon" src="img/add.png" alt="add-icon">
+                <img onclick="decreaseQuantity('${basketElement['name']}')" class="increase-decrease-icon" src="img/remove.png" alt="remove-icon">
+            </div>
+        </div>
+    `;
 }
